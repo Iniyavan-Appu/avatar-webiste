82761d2ce3be2954f70b709ed0c79c587805d667
iniyavan <iniyavanappu@gmail.com>
2022-06-27 23:48:20 +0530

    React js and bootstrap 5 installed

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const Sidebar = () => {
+  return (
+    <div>
+      Sidebar
+      <Header />
+    </div>
+  );
+};
+
+export default Sidebar;
